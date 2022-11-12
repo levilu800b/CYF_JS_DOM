7307d47b35c19760726254d03914dd855faeef3c
levilu800b <levilu800b@gmail.com>
2022-11-12 16:32:38 +0000

    set

---
+++ Array Destructuring/Exercise1/script.js
@@ -0,0 +1,9 @@
+// We have the names of the Simpsons family stored in an array called familyMembers. Create variables to store the names of the family members so that the console.log will print out the following message: In the Simpsons family, Homer and Marge are the parents. Bart is the son, Lisa is the daughter, and Maggie is the baby.
+
+let familyMembers = ['Homer', 'Marge', 'Bart', 'Lisa', 'Maggie'];
+
+// Create variables to store the values here
+
+console.log(
+	`In the Simpsons family, ${dad} and ${mum} are the parents. ${son} is the son, ${daughter} is the daughter, and ${baby} is the baby.`,
+);
---
+++ Array Destructuring/Exercise2/script.js
@@ -0,0 +1,12 @@
+// Rewrite the code below to use array destructuring instead of assigning each value to a variable
+
+let sentence = ['Hello', 'Code', 'Your', 'Future'];
+
+let firstWord = item[0];
+let secondWord = item[1];
+let thirdWord = item[2];
+let fourthWord = item[3];
+
+console.log(
+	`FirstWord: ${firstWord}, SecondWord: ${secondWord}, ThirdWord: ${thirdWord}, FourthWord: ${fourthWord},`,
+);
