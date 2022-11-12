d4e4e2f445f3cbbdb394bd265aa8e2338d82f29c
levilu800b <levilu800b@gmail.com>
2022-11-12 15:40:26 +0000

    Array Destructing

---
+++ Array Destructuring/script.js
@@ -0,0 +1,12 @@
+// Array Destructuring
+
+let array = ['John', 'James', 'Saleh'];
+// const firstIndex = array[0];
+// const secondIndex = array[1];
+// const thirdIndex = array[2];
+
+const [firstIndex, secondIndex, thirdIndex] = array;
+
+console.log(
+	`These are the name of the array ${firstIndex}, ${secondIndex}, and ${thirdIndex}`,
+);
--- scope/script.js
+++ scope/script.js
@@ -1,45 +1,36 @@
 // variable scope
 
-// const totalMoney = 100;
+const totalMoney = 100;
 
-// function customer() {
-//     const totalMoney = 33;
+function customer() {
+    const totalMoney = 33;
 
-//     if (totalMoney > 0) {
-//         const totalMoney = 10;
-//         console.log(totalMoney);
-//     }
-// }
+    if (totalMoney > 0) {
+        const totalMoney = 10;
+        console.log(totalMoney);
+    }
+}
 
-// customer();
+customer();
 
-// console.log(totalMoney);
+console.log(totalMoney);
 
 // scope
 // difference between var, let and const
 
-// const first_count = 3;
+const first_count = 3;
 
-// function GreatBritain(params) {
-//     let second_count = 4;
-//     first_count = 4
-//     console.log();
-//     if (second_count > 3) {
-//         let third_count = 8;
-//     }
-// }
+function GreatBritain(params) {
+    let second_count = 4;
+    first_count = 4
+    console.log();
+    if (second_count > 3) {
+        let third_count = 8;
+    }
+}
 
-// GreatBritain();
-// console.log(first_count);
+GreatBritain();
+console.log(first_count);
 
-// Array Destructuring
 
-let array = ["John", "James", "Saleh"];
-// const firstIndex = array[0];
-// const secondIndex = array[1];
-// const thirdIndex = array[2];
-
-const [firstIndex, secondIndex, thirdIndex] = array;
-
-console.log(`These are the name of the array ${firstIndex}, ${secondIndex}, and ${thirdIndex}`);
 
