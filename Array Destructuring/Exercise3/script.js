4e5b013799e5acfe54543656702c225f2d4ba3a3
levilu800b <levilu800b@gmail.com>
2022-11-12 16:33:57 +0000

    set

---
+++ Array Destructuring/Exercise3/script.js
@@ -0,0 +1,11 @@
+// We have a function called sumAndMultiply. It will take two numbers and then return an array where the first number is the sum of the two numbers, and the second number is the multiplication of the two numbers.
+
+function sumAndMultiply(a, b) {
+	return [a + b, a * b];
+}
+
+// Assign variables using array destructuring on this line
+
+console.log(
+	`The sum of the two numbers is ${sum}, the multiplication of the two numbers is ${multiply}`,
+);
